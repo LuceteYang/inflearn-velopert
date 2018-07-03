1c4f68a6497f5ec4408c4f98ca2a961e44edee29
sanghwan <jae6120@naver.com>
2018-07-03 22:29:59 +0900

    input 상태 관리하기

--- src/MyName.js
+++ src/MyName.js
@@ -4,6 +4,15 @@ class Myname extends Component{
 	static defaultProps= { //더 최신 문법 번들링하면 결국 밑의 코드와 같음
 		englishName:'영어이름'
 	}
+	state = {
+		name:'',
+		phone:''
+	}
+	handleChange = (e)=>{
+		this.setState({
+			[e.target.name] :e.target.value
+		})
+	}
 	render(){
 		const name = "Sanghwan"
 		return (
@@ -12,6 +21,18 @@ class Myname extends Component{
 				<h1>
 					{this.props.englishName}
 				</h1>
+				<form>
+				<input placeholder="이름" 
+				name="name"
+				onChange = {this.handleChange} 
+				value={this.state.name}/>
+				{this.state.name}
+				<input placeholder="전화번호" 
+				name="phone"
+				onChange = {this.handleChange} 
+				value={this.state.phone}/>
+				{this.state.phone}
+				</form>
 			</div>
 		)
 	}
